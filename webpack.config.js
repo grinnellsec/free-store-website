093ffc507780eaf602b252012a8ff498bc7469e3
gold-leo <168613134+gold-leo@users.noreply.github.com>
2025-01-25 16:30:57 -0600

    Added image loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,13 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
     ],
   },
 };
