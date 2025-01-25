0c6342206656e85e0dcd09b7138929ebd8c4cee9
gold-leo <168613134+gold-leo@users.noreply.github.com>
2025-01-25 16:09:34 -0600

    Added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// Use npx webpack --config webpack.config.js to build.
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
