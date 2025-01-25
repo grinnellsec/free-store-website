f9fa29ddf321a48e156cd436b67a44fa4a07987b
gold-leo <168613134+gold-leo@users.noreply.github.com>
2025-01-24 21:33:59 -0600

    Added manifest.build development configuration

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+import Manifest from '@mnfst/sdk'
+
+// Initialize client with default backend URL: http://localhost:1111.
+const manifest = new Manifest()
+
+// Get all cats.
+const cats = await manifest.from('cats').find()
+
+console.log(cats);
