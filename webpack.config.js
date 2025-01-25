77975e0a0a265f20070d6d024f6b11b9fc7d237f
gold-leo <168613134+gold-leo@users.noreply.github.com>
2025-01-25 17:32:54 -0600

    Added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,28 @@
 // Use 'npx webpack --config webpack.config.js' or 'npm run build' to build.
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'production',
+
+  entry: {
+    index: './src/index.js'
   },
-  
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Free Store',
+      template: "./src/index.html",
+    }),
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+
   // For documentation, look here https://webpack.js.org/guides/asset-management/.
   module: {
     rules: [
