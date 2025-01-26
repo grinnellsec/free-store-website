48e45c91b8438b1963ed24089878fde92c2b7714
gold-leo <168613134+gold-leo@users.noreply.github.com>
2025-01-25 19:17:31 -0600

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import Manifest from '@mnfst/sdk'
+import Manifest from '@mnfst/sdk';
 import './style.css';
 
 // Initialize client with default backend URL: http://localhost:1111.
-const manifest = new Manifest()
+const manifest = new Manifest();
 
-// Get all cats.
-const cats = await manifest.from('cats').find()
+// Get all items.
+const items = await manifest.from('items').find();
 
-console.log(cats);
+// console.log(items.data);
+
+for (let item of items.data) {
+  console.log(item.name);
+}
