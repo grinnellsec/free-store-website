cd2746e0aa284926b3daad2fec30b1917524ae8b
gold-leo <168613134+gold-leo@users.noreply.github.com>
2025-01-28 22:02:31 -0600

    Implemented JavaScript show/hide functions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import Manifest from '@mnfst/sdk';
 import './style.css';
 
+window.showPopup = () => {
+  document.getElementById("popup").style.display = "block";
+}
+
+window.hidePopup = () => {
+  document.getElementById("popup").style.display = "none";
+} 
+
+const itemList = document.getElementById("item-list");
+
 // Initialize client with default backend URL: http://localhost:1111.
 const manifest = new Manifest();
 
 // Get all items.
-const items = await manifest.from('items').find();
+// const items = await manifest.from('items').find();
 
 // console.log(items.data);
 
-for (let item of items.data) {
-  console.log(item.name);
-}
+// for (let item of items.data) {
+//   console.log(item.name);
+// }
 
-const itemList = document.getElementById("item-list");
