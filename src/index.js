ad2d584e041336b5ee2184bb1f7cf678f26fd19e
gold-leo <168613134+gold-leo@users.noreply.github.com>
2025-01-29 21:40:26 -0600

    Added cutomization

--- src/index.js
+++ src/index.js
@@ -3,18 +3,33 @@ import './style.css';
 
 const itemList = document.getElementById("item-list");
 
-// ITEM functions
-async function addItem() {
+// ITEM functions --------------------------------------
+
+/**
+ * Adds an item to the item-list.
+ */
+async function addItem(name, num) {
+  // Create a new list element (not in the DOM yet)
   let newItem = document.createElement('li');
+  // Load item.html
   const resp = await fetch("item.html");
+  // Get the text from the file
   const html = await resp.text();
+  // Insert the html from item.html between the list element
   newItem.insertAdjacentHTML("beforeend", html);
+  // Add defining characteristics
+  newItem.setAttribute('id', num);
+  newItem.firstElementChild.firstElementChild.textContent=name;
+  // Add the list element to the DOM
   itemList.appendChild(newItem);
 }
 
-window.addItem = () => {addItem();} // Dev Tool!!!
+/**
+ * 
+ */
 
-// POPUP Functions
+
+// POPUP Functions --------------------------------------
 window.showPopup = () => {
   document.getElementById("popup").style.display = "block";
 }
@@ -23,17 +38,18 @@ window.hidePopup = () => {
   document.getElementById("popup").style.display = "none";
 } 
 
-
+// DEV TOOLS --------------------------------------
+window.addItem = () => {addItem();}
 
 // Initialize client with default backend URL: http://localhost:1111.
 const manifest = new Manifest();
 
 // Get all items.
-// const items = await manifest.from('items').find();
+const items = await manifest.from('items').find();
 
-// console.log(items.data);
+console.log(items.data);
 
-// for (let item of items.data) {
-//   console.log(item.name);
-// }
+for (let item of items.data) {
+  console.log(item.name);
+}
 
